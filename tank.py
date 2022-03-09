11d07f86ed1cf616fad382bcacfc8c5f6a07fdd1
Emanoel da Conceição Cardoso Junior <65429505+EmanoelCardosoJr@users.noreply.github.com>
2022-03-09 02:07:37 -0400

    no functions or definitions yet

---
+++ tank.py
@@ -0,0 +1,15 @@
+import config
+import pygame
+
+pygame.init()
+
+conf = config
+
+sprite_1 = pygame.image.load("img//Tank sprite 1.png")
+sprite_2 = pygame.image.load("img//Tank sprite 2.png")
+sprite_3 = pygame.image.load("img//Tank sprite 3.png")
+sprite_4 = pygame.image.load("img//Tank sprite 4.png")
+
+
+# Needs forward(), rotate(), dmg_spin(), rollback(), collision()
+
