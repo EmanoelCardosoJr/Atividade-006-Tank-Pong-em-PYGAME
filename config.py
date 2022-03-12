23618d6b6b923a7953c2cf498e9a9b6c5f683416
Emanoel da Conceição Cardoso Junior <65429505+EmanoelCardosoJr@users.noreply.github.com>
2022-03-11 20:45:11 -0400

    ball tweaks

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import pygame
 
 pygame.init()
-#screen
+# screen
 screen_width = 700
 screen_height = 800
-#time
-time_counter = "2".rjust(3)
-time_text = 2
-#score
+# time
+time_text = "3".rjust(3)
+time_counter = 3
+# score
 score_p_1 = 0
 score_p_2 = 0
 font = pygame.font.Font("dependencies/EightBit Atari-Thick.ttf", 34)
-#tank constants
+# tank
 tank1_speed_x = 45
 tank1_speed_y = 350
 GREEN = (0, 50, 0)
@@ -20,3 +20,11 @@ BLUE = (0, 0, 100)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (250, 150, 0)
+# ball
+ball_y = 10
+ball_x = 10
+ball_mx = 0
+ball_my = 0
+shoot = False
+hit = False
+timer_on = False
