3c14dc17a8845bfdc6d6ba191261e3dbd02a405d
Emanoel da Conceição Cardoso Junior <65429505+EmanoelCardosoJr@users.noreply.github.com>
2022-03-14 09:36:20 -0400

    merge with score

--- config.py
+++ config.py
@@ -7,9 +7,12 @@ screen_height = 800
 # time
 time_text = "5".rjust(3)
 time_counter = 5
+time_text2 = "5".rjust(3)
+time_counter2 = 5
 # score
 score_p_1 = 0
 score_p_2 = 0
+font = pygame.font.Font("dependencies/EightBit Atari-Thick.ttf", 34)
 font_Atari = "dependencies/EightBit Atari-Thick.ttf"
 # tank
 tank1_speed_x = 45
@@ -23,10 +26,15 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (250, 150, 0)
 # ball
-ball_y = 10
-ball_x = 10
 ball_mx = 0
 ball_my = 0
+ball2_mx = 0
+ball2_my = 0
 shoot = False
+shoot2 = False
 hit = False
+hit2 = False
 timer_on = False
+timer_on2 = False
+wiggle_cont = 0
+wiggle_cont2 = 0
