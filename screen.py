885bb77360e16152714b841f9506bfeb5be52306
Emanoel da Conceição Cardoso Junior <65429505+EmanoelCardosoJr@users.noreply.github.com>
2022-03-14 02:19:08 -0400

    -started with wall collision implementation

--- screen.py
+++ screen.py
@@ -3,13 +3,15 @@ from pygame.locals import *
 import config
 from sys import exit
 import ball
+import obstacles
+from random import randint
 conf = config
 pygame.init()
 
 screen = pygame.display.set_mode((conf.screen_height, conf.screen_width))
 ball1 = ball.create_ball(conf.screen_height+100, conf.screen_width+100)
 pygame.time.set_timer(pygame.USEREVENT, 1000)
-tank_sprite = "img/player1_05.png"
+tank_sprite = "img/player1_06.png"
 tank_test = pygame.image.load(tank_sprite)
 pygame.display.set_caption('Combat')
 clock = pygame.time.Clock()
@@ -24,7 +26,7 @@ while True:
             exit()
         if conf.timer_on:
                 if event.type == pygame.USEREVENT:
-                    conf.time_counter -= 1
+                    conf.time_counter -= 0
                     conf.time_text = (
                         str(conf.time_counter).rjust(3)
                         if conf.time_counter > 0
@@ -54,42 +56,14 @@ while True:
     pygame.draw.line(screen, conf.YELLOW, (0, 700), (800, 700), 80)  # Chão
 
     pygame.draw.line(screen, conf.YELLOW, (0, 60), (800, 60), 20)  # Teto
+    #
+    obstacles.draw_obstacles()
 
-    pygame.draw.rect(screen, conf.YELLOW, (385, 621, 35, 40))  # Bloco do meio baixo
+    for obs in obstacles.obstacles_list:
+        if obs.colliderect(ball1):
+                conf.ball_mx *= -1 if randint(-3, 3) > 0 else 1
+                conf.ball_my *= -1 if randint(-3, 3) > 0 else 1
 
-    pygame.draw.rect(screen, conf.YELLOW, (385, 71, 35, 40))  # Bloco do meio cima
-
-    pygame.draw.rect(screen, conf.YELLOW, (125, 160, 70, 25))  # Bloco superior esquerda
-
-    pygame.draw.rect(screen, conf.YELLOW, (595, 160, 70, 25))  # Bloco superior direito
-
-    pygame.draw.rect(screen, conf.YELLOW, (125, 525, 70, 25))  # Bloco inferior esquerdo
-
-    pygame.draw.rect(screen, conf.YELLOW, (595, 525, 70, 25))  # Bloco inferior esquerdo
-
-    pygame.draw.rect(screen, conf.YELLOW, (555, 335, 40, 40))  # Bloco direito
-
-    pygame.draw.rect(screen, conf.YELLOW, (200, 335, 40, 40))  # Bloco esquerdo
-
-    pygame.draw.rect(screen, conf.YELLOW, (110, 255, 25, 200))  # Poste da tabala esquerda
-    pygame.draw.rect(screen, conf.YELLOW, (80, 255, 30, 25))
-    pygame.draw.rect(screen, conf.YELLOW, (80, 430, 30, 25))
-
-    pygame.draw.rect(screen, conf.YELLOW, (270, 215, 70, 25))  # Bloco meio superior esquerdo
-    pygame.draw.rect(screen, conf.YELLOW, (270, 235, 25, 25))
-
-    pygame.draw.rect(screen, conf.YELLOW, (270, 455, 70, 25))  # Bloco meio inferior esquerdo
-    pygame.draw.rect(screen, conf.YELLOW, (270, 435, 25, 25))
-
-    pygame.draw.rect(screen, conf.YELLOW, (655, 255, 30, 200))  # Poste da tabela direita
-    pygame.draw.rect(screen, conf.YELLOW, (685, 255, 30, 25))
-    pygame.draw.rect(screen, conf.YELLOW, (685, 430, 30, 25))
-
-    pygame.draw.rect(screen, conf.YELLOW, (460, 215, 70, 25))  # Bloco meio superior direito
-    pygame.draw.rect(screen, conf.YELLOW, (505, 235, 25, 25))
-
-    pygame.draw.rect(screen, conf.YELLOW, (460, 455, 70, 25))  # Bloco meio inferior direito
-    pygame.draw.rect(screen, conf.YELLOW, (505, 435, 25, 25))
 
     # colisão e movimento da bola
 
@@ -107,7 +81,7 @@ while True:
     # ball collision with tank rect
     if tank.colliderect(ball1):
         # use whatever tank collision functions with the ball you have here
-        ball.end_ball(ball1, conf.ball_mx, conf.ball_my)
+        conf.hit = True
     # keyboard inputs
     if pygame.key.get_pressed()[K_w]:
 
